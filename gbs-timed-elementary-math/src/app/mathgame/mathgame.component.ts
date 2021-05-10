a4f96d45abaaf17bf92050a2e4ee52f53043b984
PaulClefUbeCoding <72354974+PaulClefUbeCoding@users.noreply.github.com>
2021-05-09 23:00:02 -0700

    Game Core Working and Doing Result Page

--- gbs-timed-elementary-math/src/app/mathgame/mathgame.component.ts
+++ gbs-timed-elementary-math/src/app/mathgame/mathgame.component.ts
@@ -1,7 +1,7 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { MatStepper } from '@angular/material/stepper';
 import { timer } from 'rxjs';
@@ -34,6 +34,7 @@ import { timer } from 'rxjs';
   ],
 })
 export class MathgameComponent implements OnInit {
+  @ViewChild('stepper') private myStepper: MatStepper;
 
   title: string;
   public isViewable: boolean;
@@ -45,6 +46,11 @@ export class MathgameComponent implements OnInit {
   operations: string[] = ['Addition', 'Subtraction', 'Multiplication', 'Division'];
   selectedDigit: number;
   selectedChallenge: number;
+  selectedOperation: string;
+  convertedOperation: string;
+  correct: boolean;
+  victoryMessage: string;
+  challengeLeft: number;
 
   intervalId = 0;
   message = '';
@@ -65,20 +71,22 @@ export class MathgameComponent implements OnInit {
     this.formGroup1 = new FormGroup ({
       gameInformation: new FormGroup({
         name: new FormControl(null,),
-        digit: new FormControl(null,),
+        selectedDigit: new FormControl(null,),
         timer: new FormControl(null, ),
-        challenge: new FormControl(null, ),
-        operation: new FormControl(null, ),
+        answer: new FormControl(null, ),
       }),
     });
   }
 
   startTimer() {
     console.log("inside start timer");
-    console.log("Digit selected:" + this.formGroup1.value.gameInformation.digit);
-    console.log("Digit selected:" + this.formGroup1.value.gameInformation.challenge);
+    console.log("Digit selected:" + this.selectedDigit);
+    console.log("Challenge selected:" + this.selectedChallenge);
+    console.log("Operation selected: " + this.selectedOperation)
     this.generateTheNumbers();
+    this.convertOperation();
     this.seconds = this.formGroup1.value.gameInformation.timer;
+    this.challengeLeft = this.selectedChallenge;
     this.countDownTimer(); 
   }
   stopTimer() {
@@ -97,17 +105,81 @@ export class MathgameComponent implements OnInit {
       if(this.seconds === 0) {
         this.message = 'Times up! Next Challenge!';
       } else {
-        if (this.seconds < 0) { this.seconds = this.formGroup1.value.gameInformation.timer;} //reset
-        this.message = this.seconds+ 'seconds left!'
+        if (this.seconds < 0) { 
+          this.seconds = this.formGroup1.value.gameInformation.timer;
+          this.victoryMessage = "";
+          this.generateTheNumbers();
+          this.challengeLeft-=1;
+          if(this.challengeLeft == 0) {
+            this.myStepper.next();
+          }
+        } //reset
+        this.message = this.seconds+ ' seconds left!';
+        this.victoryMessage = "";
       }
     }, 1000);
   }
 
   public generateTheNumbers() {
-    // if()
-    // this.number1 = Math.random()*
+    if(this.selectedDigit == 3) {
+      this.number1 = Math.floor(Math.random()*(999-100+1)+100);
+      this.number2 = Math.floor(Math.random()*(999-100+1)+100);
+    } else if(this.selectedDigit == 2) {
+      this.number1 = Math.floor(Math.random()*90+10);
+      this.number2 = Math.floor(Math.random()*90+10);
+    } else {
+      this.number1 = Math.floor(Math.random()*10);
+      this.number2 = Math.floor(Math.random()*10);
+    }
   }
 
+  public convertOperation() {
+    if(this.selectedOperation == "Addition") {
+      this.convertedOperation = "+";
+    } else if(this.selectedOperation == "Subtraction") {
+      this.convertedOperation = "-";
+    } else if(this.selectedOperation == "Multiplication") {
+      this.convertedOperation = "x";
+    } else {
+      this.convertedOperation = "/";
+    }
+  }
+
+  public answerButtonClick() {
+    this.correct = false;
+    if(this.selectedOperation == "Addition") {
+      if(this.formGroup1.value.gameInformation.answer == (this.number1+this.number2)) {
+        this.correct = true;
+      }
+    } 
+    else if(this.selectedOperation == "Subtraction") {
+      if(this.formGroup1.value.gameInformation.answer == (this.number1-this.number2)) {
+        this.correct = true;
+      }
+    } 
+    else if(this.selectedOperation == "Multiplication") {
+      if(this.formGroup1.value.gameInformation.answer == (this.number1*this.number2)) {
+        this.correct = true;
+      }
+    } 
+    else {
+      if(this.formGroup1.value.gameInformation.answer == (this.number1/this.number2)) {
+        this.correct = true;
+      }
+    }
+
+    if(this.correct) {
+      this.showVictory();
+    } else {
+      this.victoryMessage = "Not quiete right!"
+    }
+
+  }
+
+  public showVictory() {
+    this.victoryMessage = "Nice one!";
+    this.seconds = 1;
+  }
 
 }
 
